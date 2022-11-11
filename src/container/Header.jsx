4bfd0df64e2e127a4561e6d573e312f4a0ef1cfa
haviles04 <haviles04@gmail.com>
2022-11-11 18:21:13 -0500

    added state to search

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Searchbar from "../components/Searchbar";
 
-const Header = () => {
+const Header = ({searchTerm, setSearchTerm}) => {
+  
   return (
     <div className="w-full h-[280px] bg-hero bg-cover flex justify-center">
       <div className="w-[600px] flex-col justify-center ">
-        <div className="text-white text-3xl font-medium pt-[30px] flex justify-center">
+        <div className="text-white text-3xl font-bold pt-[30px] flex justify-center">
           <h1>IP Address Tracker</h1>
         </div>
         <div className="pt-[28px] flex justify-center">
-        <Searchbar />
+        <Searchbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </div>
     </div>
