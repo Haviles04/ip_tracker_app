3101422ed6cce6bfb271d8a5a3bcebde3b1d3b71
haviles04 <haviles04@gmail.com>
2022-11-10 19:05:24 -0500

    initalized project, started css

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Searchbar = () => {
+  return (
+    <div>
+        <input 
+        className='bg-arrow h-[58px] w-[555px] rounded-xl outline-none '
+        type='text'></input>
+    </div>
+  )
+}
+
+export default Searchbar
---
+++ src/container/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Searchbar from "../components/Searchbar";
+
+const Header = () => {
+  return (
+    <div className="w-full h-[280px] bg-hero bg-cover flex justify-center">
+      <div className="w-[600px] flex-col justify-center ">
+        <div className="text-white text-3xl font-medium pt-[30px] flex justify-center">
+          <h1>IP Address Tracker</h1>
+        </div>
+        <div className="pt-[28px] flex justify-center">
+        <Searchbar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
