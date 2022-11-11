6169d6ac81338c08a89c5285b3c602ac4afa2685
haviles04 <haviles04@gmail.com>
2022-11-11 18:22:17 -0500

    add apirequest for inital information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css';
-import Header from './container/Header';
+import "./App.css";
+import Header from "./container/Header";
+import React, { useEffect, useState } from "react";
+import Infobar from "./components/Infobar";
+import Map from "./components/Map";
 
 function App() {
-  
-  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [info, setInfo] = useState(null);
+
+  let apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    if (!info) {
+      fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          setInfo(response);
+        });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
-      <Header />
+    <div className="w-full h-full flex-col justify-center">
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="flex justify-center">
+        <Infobar info={info} />
+      </div>
+      <Map />
     </div>
   );
 }
