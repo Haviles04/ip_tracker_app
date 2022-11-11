d1774359fd7dac1e49547c93a824107197363886
haviles04 <haviles04@gmail.com>
2022-11-10 19:04:46 -0500

    Initalized project,

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+
+    backgroundImage: {
+      'hero': "url('../src/assets/pattern-bg.png')",
+      'arrow': "url('../src/assets/icon-arrow.svg)",
+
+    extend: {
+
+        
+      }
+    },
+  },
+  plugins: [],
+}
