04722741c16e8ae8bd072d0719bf882dd19ae648
haviles04 <haviles04@gmail.com>
2022-11-11 18:20:57 -0500

    built out searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
+import iconArrow from "../assets/icon-arrow.svg";
 
-const Searchbar = () => {
+const Searchbar = ({searchTerm, setSearchTerm}) => {
+  
+console.log(searchTerm);
+  
+const handleSubmit= (search) => {
+
+};
+  
   return (
     <div>
-        <input 
-        className='bg-arrow h-[58px] w-[555px] rounded-xl outline-none '
-        type='text'></input>
+      <form>
+        <label className="h-[58px] w-[555px] rounded-xl flex align-center overflow-hidden">
+          <input
+            className="h-[58px] w-[497px] pl-5 font-medium outline-none"
+            placeholder="Search for any IP address or domain"
+            type="text"
+            onChange={(e) => {
+              e.preventDefault(); 
+              setSearchTerm(e.target.value);  
+            }}
+          />
+          <input
+            type="image"
+            src={iconArrow}
+            alt="arrow"
+            onClick={handleSubmit(searchTerm)}
+            className="h-[58px] w-[58px] bg-black object-none"
+          />
+        </label>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
